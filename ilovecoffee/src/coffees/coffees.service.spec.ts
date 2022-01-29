cff132b39a7a4addf233c3eb7256e46d64dab111
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2022-01-29 19:03:39 +0200

    add  unit  tests

--- ilovecoffee/src/coffees/coffees.service.spec.ts
+++ ilovecoffee/src/coffees/coffees.service.spec.ts
@@ -1,12 +1,28 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
+import coffeesConfig from './config/coffees.config';
+import { COFFEE_BRANDS } from './coffee.constants';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: Connection, useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+        { provide: ConfigService, useValue: { get: () => true } },
+        { provide: COFFEE_BRANDS, useValue: '' },
+        { provide: coffeesConfig.KEY, useValue: '' },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
